16805c176d4c18b4baadef5b7aac26cab5aa4d58
unknown <xiaohw@hffss.com>
2019-05-05 11:15:31 +0800

    'log1'

---
+++ commonjs/a.js
@@ -0,0 +1,10 @@
+const {add,result} = require('./b')
+
+const _ = require('lodash')
+
+
+console.log(add(10,20))
+console.log(result(10,10))
+
+const arr = _.concat([1,2],3)
+console.log('arr...',arr)
---
+++ commonjs/b.js
@@ -0,0 +1,10 @@
+function  add (a,b){
+    return a+b
+}
+function  result (a,b){
+    return a*b
+}
+module.exports = {
+    add:add,
+    result:result,
+}
